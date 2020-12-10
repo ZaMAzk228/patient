13bc709e8625a408ce7c75c6078f3a9fa1ff7e6d
thefirefox <wtfthefirefox@gmail.com>
2020-12-11 00:13:07 +0300

    fixed previous commit

--- src/components/pages/firstPages.js
+++ src/components/pages/firstPages.js
@@ -5,17 +5,13 @@ import './firstPages.css';
 
 const FirstPages = () => {
     return (
-        <div className="firstPages">
-            <div className="firstPages__container">
-                <div className="firstPages__wrapper">
-                    <img className="firstPages__img" src="../../../assets/img/doctor.png" />
-                    <div className="firstPages__content-wrapper">
-                        <Link to="/register" className="firstPages__btn">Зарегистрироваться</Link>
-                        <h1 className="firstPages__text">ИЛИ</h1>
-                        <Link to="/login" className="firstPages__btn">Войти</Link>
-                    </div>
-                </div>
-            </div> 
+        <div className="firstPages__wrapper">
+            <img className="firstPages__img" src="../../../assets/img/doctor.png" />
+            <div className="firstPages__content-wrapper">
+                <Link to="/register" className="firstPages__btn">Зарегистрироваться</Link>
+                <h1 className="firstPages__text">ИЛИ</h1>
+                <Link to="/login" className="firstPages__btn">Войти</Link>
+            </div>
         </div>
     )
 }
