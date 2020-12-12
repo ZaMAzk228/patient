2dd2e5adb601a6d34b08d1f370c66f65305fe9de
ZaMAzk228 <70816669+ZaMAzk228@users.noreply.github.com>
2020-12-12 17:58:09 +0300

    Add files via upload

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,11 +1,11 @@
 import FirstPages from './firstPages';
 import Register from './register';
 import Login from './login';
-// import Priem from './priem';
+import Priem from './priem';
 
 export {
   FirstPages,
   Register,
-  Login
-  // Priem
+  Login,
+  Priem
 };
